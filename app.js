9678005c8ee34d75ec2b51b5385886097c8a7ab1
Venkat S <venkat.s@spritle.com>
2017-05-06 03:12:54 +0530

    direct uri

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ const app = express();
  * Connect to MongoDB.
  */
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || process.env.MONGOLAB_URI);
+mongoose.connect('mongodb://admin:password@ds133241.mlab.com:33241/heroku_bd7xw0xd');
 mongoose.connection.on('error', () => {
   console.log('%s MongoDB connection error. Please make sure MongoDB is running.', chalk.red('✗'));
   process.exit();
