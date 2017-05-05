39d76a2b34a571662560f6ccc5e31988d39ae1fd
Venkat S <venkat.s@spritle.com>
2017-05-06 05:07:38 +0530

    app js update

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ mongoose.connect(process.env.MONGODB_URI || process.env.MONGOLAB_URI);
 mongoose.connection.on('error', () => {
   console.log('%s MongoDB connection error. Please make sure MongoDB is running.', chalk.red('✗'));
   process.exit();
-});*/
+});   */
 
 /**
  * Express configuration.
