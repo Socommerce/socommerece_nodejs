96916dde6aad6c13c252c0545e1aa066d8398fae
Venkat S <venkat.s@spritle.com>
2017-05-06 01:52:49 +0530

    re push config

--- controllers/api.js
+++ controllers/api.js
@@ -9,10 +9,7 @@ var BaseAuth = require('basic-auth');
 
 
 
-/**
- * GET /api
- * List of API examples.
- */
+
 exports.getApi = (req, res) => {
   res.render('api/index', {
     title: 'API Examples'
