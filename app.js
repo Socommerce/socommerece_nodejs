a0c77db54c4b673ad6dc9e0ff2883376a2fddfb4
Venkat S <venkat.s@spritle.com>
2017-05-06 06:48:38 +0530

    add mongodb

--- app.js
+++ app.js
@@ -55,12 +55,12 @@ const app = express();
 /**
  * Connect to MongoDB.
  */
-/*mongoose.Promise = global.Promise;
+mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI || process.env.MONGOLAB_URI);
 mongoose.connection.on('error', () => {
   console.log('%s MongoDB connection error. Please make sure MongoDB is running.', chalk.red('✗'));
   process.exit();
-});*/
+});
 
 /**
  * Express configuration.
